6216f87a6c7ab8dfcf06772c474bfe3a21e7091d
Guilherme Max Pereira <99480109+Guimax18@users.noreply.github.com>
2023-11-20 21:36:17 -0300

    Update cadastro.js

--- src/pages/cadastro/cadastro.js
+++ src/pages/cadastro/cadastro.js
@@ -1,7 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './cadastro.scss';
 import axios from 'axios'
-import { Link } from 'react-router-dom';
+import logo from '../assets/logo img/logo.animix.png'
+import perfil from '../assets/icons/perfil.png'
+import { Link, useParams } from 'react-router-dom';
+
 
 
 export default function Cadastro() {
@@ -11,7 +14,26 @@ export default function Cadastro() {
   const [valor, setValor] = useState ('');
   const [cor, setCor] = useState ('');
   const [anime, setAnime] = useState ('');
+  const [imagem, setImagem] = useState ('');
 
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (id) {
+      buscaProduto();
+    }
+  }, [])
+
+  async function buscaProduto(){
+    let r = await axios.get('http://localhost:8400/roupa/' + id);
+    setNome(r.data.nome);
+    setCategoria(r.data.categoria);
+    setTamanho(r.data.tamanho);
+    setValor(r.data.valor);
+    setCor(r.data.cor);
+    setAnime(r.data.anime);
+    setImagem(r.data.imagem)
+  }
 
   async function salvarRoupa() {
     let body = {
@@ -23,17 +45,36 @@ export default function Cadastro() {
         anime: anime
     }
 
-   let r = await axios.post('http://localhost:8400/roupa/', body);
-   let id = r.data.id;
-   
-   alert('Roupa cadastrada. id' + id);
-
+    if (id) {
+      let r = await axios.put('http://localhost:8400/roupa/'+id, body);
+      alert('Roupa alterada');
+    }
+    else {
+      let r = await axios.post('http://localhost:8400/roupa/', body);
+      let id = r.data.id;
+      alert('Roupa cadastrada. id' + id);
+    }
   }
 
 
   return (
     <div className='pagina-cadastro'>
+
+      <div className='header'>
+      <a href="/"><img src={logo} alt="logo_principal" class="logo"/></a>
       <h1>Cadastro de Roupas</h1>
+      <a href="/Login Page - ANIMIX/Login Page - ANIMIX/login.html"><img class="perfil" src={perfil} alt="icone de perfil"/></a>
+      </div>
+
+      <img src=''/>
+
+      <div>
+      <input type='file'/>
+      </div>
+
+      <div>
+        <img src={'http://localhost:8400/' + imagem}/>
+      </div>
 
       <div className='form'>
         <div>
